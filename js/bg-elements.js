99be968d388b75df7444d386a2c6640fe224c46b
Max Local <max-local@mail.ru>
2022-09-24 18:38:18 +0300

    Пройден первый день урока - модальное окно

---
+++ js/bg-elements.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let elements = document.querySelectorAll('.set-bg');
+
+// console.log(elements);
+for (let i = 0; i < elements.length; i++) {
+    // console.dir(elements[i]);
+    let src = elements[i].dataset.setbg;
+
+    elements[i].style.backgroundImage = `url('${src}')`;
+    // console.log(src);
+}
