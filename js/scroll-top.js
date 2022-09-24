27456e02ac8e0e2e198afaa41693061f54a9e134
Max Local <max-local@mail.ru>
2022-09-24 18:54:08 +0300

    Добавлен плавный скролл

---
+++ js/scroll-top.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const topBtn = document.querySelector('#scrollToTopButton')
+// console.log(topBtn);
+
+topBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    seamless.scrollIntoView(document.querySelector(".header"), {
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+    });
+});
