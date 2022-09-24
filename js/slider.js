6397f66447a1e41f1c7f859ef85d492ea32fe80a
Max Local <max-local@mail.ru>
2022-09-24 19:14:47 +0300

    Добавлен слайдер библиотеки «Swiper slider»

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+      },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+    effect: 'fade',
+    speed: 1000
+});
