bf39de9458b84b8f9692528d8d7fac85a79f7653
Max Local <max-local@mail.ru>
2022-09-26 09:27:04 +0300

    Фильтрация и сортировка методами

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -4,13 +4,42 @@ function bgElements() {
     let elements = document.querySelectorAll('.set-bg');
 
     // console.log(elements);
-    for (let i = 0; i < elements.length; i++) {
-        // console.dir(elements[i]);
-        let src = elements[i].dataset.setbg;
+    // for (let i = 0; i < elements.length; i++) {
+    //     let src = elements[i].dataset.setbg;
+    //     elements[i].style.backgroundImage = `url('${src}')`;
+    // }
 
-        elements[i].style.backgroundImage = `url('${src}')`;
-        // console.log(src);
-    }
-};
+    elements.forEach((element) => {
+        element.style.backgroundImage = `url('${element.dataset.setbg}')`;
+    });
+
+    //* Фильтрация массива методом filter
+    // const array = [1, 2, 3, 4, 5];
+    // const newArray = array.filter((item) => {
+    //     return item <= 3;
+    // });
+    // console.log(newArray);
+
+    //* Сортировка массива и объекта методом Sort
+    // const array = [5, 6, 3, 7, 2];
+    // const arrayObj = [
+    //     {
+    //         id: 3,
+    //         value: 100
+    //     },
+    //     {
+    //         id: 2,
+    //         value: 300
+    //     },
+    //     {
+    //         id: 1,
+    //         value: 200
+    //     }
+    // ];
+    // const newArraySort = arrayObj.sort((a, b) => {
+    //     return a.id - b.id;
+    // });
+    // console.log(newArraySort);
+}
 
 bgElements();
