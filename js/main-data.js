6c47a876e06de1343cd6fd2774c9493877b8531a
Max Local <max-local@mail.ru>
2022-09-26 18:11:13 +0300

    Убрали лишние блоки в разметке, сделали сортировку по жанрам

--- js/main-data.js
+++ js/main-data.js
@@ -1,8 +1,82 @@
 const mainData = () => {
+    
+    function renderGanreList(ganres) {
+        const dropDownBlock = document.querySelector('.header__menu .dropdown');
+        
+        dropDownBlock.innerHTML = '';
 
-    function renderAnimeList(arr, genre) {
-        console.log(arr);
-        console.log(genre);
+        ganres.forEach(ganre => {
+            dropDownBlock.insertAdjacentHTML('afterbegin', `
+                <li><a href="./categories.html?ganre=${ganre}">${ganre}</a></li>
+            `)
+        })
+    }
+
+    function renderAnimeList(arr, ganres) {
+        const wrapper = document.querySelector('.product .col-lg-8');
+        
+        wrapper.innerHTML = '';
+
+        ganres.forEach((ganre) => {
+            const productBlock = document.createElement('div');
+            const listBlock = document.createElement('div');
+            const list = arr.filter(item => item.ganre === ganre);
+            
+
+            listBlock.classList.add('row');
+            productBlock.classList.add('mb-5');
+
+            productBlock.insertAdjacentHTML('afterbegin', `
+                <div class="row">
+                    <div class="col-lg-8 col-md-8 col-sm-8">
+                        <div class="section-title">
+                            <h4>${ganre}</h4>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4">
+                        <div class="btn__all">
+                            <a href="/categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
+                        </div>
+                    </div>
+                </div>
+            `)
+
+            list.forEach(item => {
+                const tagsBlock = document.createElement('ul');
+
+                item.tags.forEach(tag => {
+                    tagsBlock.insertAdjacentHTML('afterbegin', `
+                        <li>${tag}</li>
+                    `)
+                });
+                
+                // console.dir(tagsBlock);
+
+                listBlock.insertAdjacentHTML('afterbegin', `
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="product__item">
+                            <div class="product__item__pic set-bg" data-setbg="${item.image}">
+                                <div class="ep">${item.rating} / 10</div>
+                                <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
+                            </div>
+                            <div class="product__item__text">
+                                <ul>
+                                    ${tagsBlock.outerHTML}
+                                </ul>
+                                <h5><a href="/anime-details.html?itemId=${item.id}">${item.title}</a></h5>
+                            </div>
+                        </div>
+                    </div>
+                `)
+            })
+
+            productBlock.append(listBlock);
+            wrapper.append(productBlock);
+
+            wrapper.querySelectorAll('.set-bg').forEach((element) => {
+                element.style.backgroundImage = `url('${element.dataset.setbg}')`;
+            });
+        })
     }
 
     function renderTopAnime(arr) {
@@ -49,6 +123,7 @@ const mainData = () => {
             })
 
             renderAnimeList(data.anime, genres)
+            renderGanreList(genres)
         })
 
 }
